5e8fd664a0d3addd48d34b69695a3cb855a5f539
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-10-25 22:37:38 +0530

    Register UI

---
+++ register.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<title>Web Blog</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="page">
+  <h1>Web Blog</h1>
+  <div class="metanav">
+    <a href="login.php">log in</a>
+  </div>
+  <h2>Register</h2>
+  <form action="register.php" method="post">
+    <dl>
+      <dt>Username:
+      <dd><input type="text" name="username">
+      <dt>Password:
+      <dd><input type="password" name="password">
+      <dd><input type="submit" value="Login">
+    </dl>
+  </form>
+</div>
